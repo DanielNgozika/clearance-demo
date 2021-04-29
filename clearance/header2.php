97f7b482c29c0ec9d8a932a0b26d7db679c0847f
Daniel Ngozika <danielngozikac@gmail.com>
2021-04-29 08:30:26 +0100

    Path fix

--- clearance/header2.php
+++ clearance/header2.php
@@ -20,14 +20,14 @@
 
     <!-- Bootstrap and Font Awesome css -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-eOJMYsd53ii+scO/bJGFsiCZc+5NDVN2yr8+0RDqr0Ql0h+rP48ckxlpbzKgwra6" crossorigin="anonymous">
-    <link href="/clearance/css/font-awesome.css" rel="stylesheet">
-    <!-- <link href="/clearance/css/bootstrap.min.css" rel="stylesheet"> -->
+    <link href="./css/font-awesome.css" rel="stylesheet">
+    <!-- <link href="./css/bootstrap.min.css" rel="stylesheet"> -->
 
     <!-- Theme stylesheet -->
-    <link href="/clearance/css/style.default.css" rel="stylesheet" id="theme-stylesheet">
+    <link href="./css/style.default.css" rel="stylesheet" id="theme-stylesheet">
 
     <!-- Custom stylesheet - for your changes -->
-    <link href="/clearance/css/custom.css" rel="stylesheet">
+    <link href="./css/custom.css" rel="stylesheet">
 
     <!-- owl carousel css -->
 
